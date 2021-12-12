a57d41812147217986320bdc177f5b08929950a4
bendsouza2 <bendsouza2@gmail.com>
2021-12-12 19:58:24 +0000

    built a preliminary classification tree, clf_dt drew confusion matrix for clf_dt

--- tree.py
+++ tree.py
@@ -1,5 +1,9 @@
+import DecisionTree as DecisionTree
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 df = pd.read_csv('processed.cleveland.data', header=None)
 df.columns = ['age', 'sex', 'cp', 'restbp', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca',
@@ -17,7 +21,7 @@ df.columns = ['age', 'sex', 'cp', 'restbp', 'chol', 'fbs', 'restecg', 'thalach',
 #           |
 #                  (df['thal'] == '?')]))
 
-df_no_missing = df.loc[(df['ca'] != '?') & (df['thal'] != '?')] # importing all rows with no missing values
+df_no_missing = df.loc[(df['ca'] != '?') & (df['thal'] != '?')]  # importing all rows with no missing values
 
 
 X = df_no_missing.drop('hd', axis=1).copy()  # data for classification
@@ -31,3 +35,16 @@ y[non_zero_index] = 1  # setting all non-zero values equal to 1
 
 X_train, X_test = train_test_split(X_encoded, random_state=42)
 y_train, y_test = train_test_split(y, random_state=42)
+
+clf_dt = DecisionTreeClassifier(random_state=42)
+clf_dt = clf_dt.fit(X_train, y_train)
+
+# Plotting the tree
+plt.figure(figsize=(15, 7.5))
+plot_tree(clf_dt, filled=True, rounded=True, class_names=['No HD', 'Yes HD'], feature_names=X_encoded.columns)
+plt.show()
+
+# Confusion matrix
+plot_confusion_matrix(clf_dt, X_test, y_test, display_labels=['Does not have HD', 'Has HD'])
+
+
