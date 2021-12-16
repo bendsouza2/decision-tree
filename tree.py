e0a0f68501dc835ad013e853272cc2067ac03ec5
bendsouza2 <bendsouza2@gmail.com>
2021-12-16 14:23:58 +0000

    Used cross-validation to find the optimal value for alpha

--- tree.py
+++ tree.py
@@ -1,8 +1,10 @@
-import DecisionTree as DecisionTree
+# import DecisionTree as DecisionTree
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
@@ -59,13 +61,13 @@ ccp_alphas = ccp_alphas[:-1]  # removing maximum value
 
 dta = []  # array to put decision trees into
 
-#  Creating a decision tree for each value of alpha
+# Creating a decision tree for each value of alpha
 for alpha in ccp_alphas:
     clf_dt = DecisionTreeClassifier(random_state=0, ccp_alpha=alpha)
     clf_dt.fit(X_train, y_train)
     dta.append(clf_dt)
 
-#  Graphing the accuracy of the trees for the training and testing dataset as a function of alpha
+# Graphing the accuracy of the trees for the training and testing dataset as a function of alpha
 train_scores = [clf_dt.score(X_train, y_train) for clf_dt in dta]
 test_scores = [clf_dt.score(X_test, y_test) for clf_dt in dta]
 
@@ -76,7 +78,31 @@ ax.set_title('Accuracy for different values of alpha')
 ax.plot(ccp_alphas, train_scores, marker='o', label='train', drawstyle='steps-post')
 ax.plot(ccp_alphas, test_scores, marker='o', label='test', drawstyle='steps-post')
 ax.legend()
+
+
+# Cross-validation (5-fold)
+clf_dt = DecisionTreeClassifier(random_state=42, ccp_alpha=0.016)
+scores = cross_val_score(clf_dt, X_train, y_train, cv=5)
+df = pd.DataFrame(data={'tree': range(5), 'accuracy': scores})
+df.plot(x='tree', y='accuracy', marker='o', linestyle='--')
+
+
+# Optimal alpha with cross validation
+alpha_loop_vals = []  # array to store results of each fold
+for alpha in ccp_alphas:
+    clf_dt = DecisionTreeClassifier(random_state=0, ccp_alpha=alpha)
+    scores = cross_val_score(clf_dt, X_train, y_train, cv=5)
+    alpha_loop_vals.append([alpha, np.mean(scores), np.std(scores)])
+
+# Plot the graph
+alpha_results = pd.DataFrame(alpha_loop_vals, columns=['alpha', 'mean_accuracy', 'std'])
+alpha_results.plot(x='alpha', y='mean_accuracy', yerr='std', marker='o', linestyle='--')
 plt.show()
 
+# Finding optimal value for alpha
+ideal_ccp_alpha = alpha_results[(alpha_results['alpha'] > 0.014)
+              &
+              (alpha_results['alpha'] < 0.015)]['alpha']
+ideal_ccp_alpha = float(ideal_ccp_alpha)  # converting ideal_ccp_alpha from series to float
 
 
